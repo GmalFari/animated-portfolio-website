1108487a46ff5045dd203e3736e25654fc23f8da
GmalFari <gmalfari926@gmail.com>
2023-07-22 13:33:32 +0300

    add drawsvg

--- src/components/Home/Logo/Logo.jsx
+++ src/components/Home/Logo/Logo.jsx
@@ -7,29 +7,31 @@ const Logo = () => {
   const outlineLogoRef = useRef()
   const solidLogoRef = useRef()
   
-  useEffect(()=>{
-      let ctx = gsap.context(() => {
-          gsap.to(bgRef.current,{
-              duration:1,
-              opacity:1,
-          })
-         
-    
-          gsap.fromTo(
-            solidLogoRef.current,{
-              opacity:0,
-            },
-            {
-              opacity:1,
-              delay:4,
-              duration:4
-            }
-          )
-         }, bgRef) // <- IMPORTANT! Scopes selector text
-  
-      return () => ctx.revert(); 
+  useEffect(() => {
 
-    },[])
+    gsap
+      .timeline()
+      .to(bgRef.current, {
+        duration: 1,
+        opacity: 1,
+      })
+      .from(outlineLogoRef.current, {
+        drawSVG: 0,
+        duration: 20,
+      })
+
+    gsap.fromTo(
+      solidLogoRef.current,
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        delay: 4,
+        duration: 4,
+      }
+    )
+  }, [])
   return (
 
     <div ref={bgRef} className='logo-container'>
